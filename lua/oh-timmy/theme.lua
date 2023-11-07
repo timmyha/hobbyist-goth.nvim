c8fb534e9ba9297d72e0e0e5e07bc5cc6ce12e38
timmyha <timhansher@gmail.com>
2023-11-07 11:17:20 -0600

    readme

--- lua/oh-timmy/theme.lua
+++ lua/oh-timmy/theme.lua
@@ -443,7 +443,6 @@ M.plugins = {
     -----------------------------------------
     --   Nvim-tree: github.com/kyazdani42/nvim-tree.lua
     -----------------------------------------
-    NvimTreeFolderIcon = { fg = colors.orange },
     NvimTreeEmptyFolderName = { fg = colors.yellow, style = "italic" },
     NvimTreeCursorLine = { fg = colors.line_fg, bg = colors.black1 },
     NvimTreeExecFile = { fg = colors.green },
